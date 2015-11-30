28a9b1e307882a224499494c73a1f3a805a0827f
Vlad Z <vladislavz1991@gmail.com>
2015-11-30 13:39:21 -0500

    size fix

--- JSQMessagesViewController/Layout/JSQMessagesBubblesSizeCalculator.m
+++ JSQMessagesViewController/Layout/JSQMessagesBubblesSizeCalculator.m
@@ -128,10 +128,10 @@
 
         //  add extra 2 points of space (`self.additionalInset`), because `boundingRectWithSize:` is slightly off
         //  not sure why. magix. (shrug) if you know, submit a PR
-        CGFloat verticalInsets = verticalContainerInsets + verticalFrameInsets + self.additionalInset;
+        CGFloat verticalInsets = verticalContainerInsets + verticalFrameInsets + 5.0;
 
         //  same as above, an extra 2 points of magix
-        CGFloat finalWidth = MAX(stringSize.width + horizontalInsetsTotal, self.minimumBubbleWidth) + self.additionalInset;
+        CGFloat finalWidth = MAX(stringSize.width + horizontalInsetsTotal, self.minimumBubbleWidth) + 5.0;
 
         finalSize = CGSizeMake(finalWidth, stringSize.height + verticalInsets);
     }
